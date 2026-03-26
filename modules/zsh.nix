487d54038674a4ac5ad65a60d136c4ef1e449217
Cook3DCookie <147427534+Cook3DCookie@users.noreply.github.com>
2026-03-26 22:17:02 +0100

    Remove conda/miniforge config

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -13,27 +13,7 @@
         echo -ne "\033]0;Rosetta Terminal (x86_64)\007"
       fi
 
-      # >>> conda initialize >>>
-      # !! Contents within this block are managed by 'conda init' !!
-      __conda_setup="$('/Users/lukas/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-          eval "$__conda_setup"
-      else
-          if [ -f "/Users/lukas/miniforge3/etc/profile.d/conda.sh" ]; then
-            . "/Users/lukas/miniforge3/etc/profile.d/conda.sh"
-          else
-            export PATH="/Users/lukas/miniforge3/bin:$PATH"
-          fi
-      fi
-      unset __conda_setup
-
-      if [ -f "/Users/lukas/miniforge3/etc/profile.d/mamba.sh" ]; then
-          . "/Users/lukas/miniforge3/etc/profile.d/mamba.sh"
-      fi
-      # <<< conda initialize <<<
-
       exec fish
-
     '';
 
     shellAliases = {
