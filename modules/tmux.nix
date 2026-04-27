e227732ea9c95d4e64df1d69984a37360437ebb8
Cook3DCookie <147427534+Cook3DCookie@users.noreply.github.com>
2026-04-27 19:50:40 +0200

    Configure tmux

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -4,6 +4,7 @@
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
+    keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
       resurrect # configuration needed for both
       continuum
@@ -11,6 +12,17 @@
     extraConfig = ''
       set-option -ga terminal-overrides ",xterm-256color:RGB"
       set -g mouse on
+
+      set -g prefix C-space
+      unbind C-b
+      bind C-space send-prefix
+
+      set -g base-index 1
+      set -g pane-base-index 1
+      set -g renumber-windows on
+      
+      set -g history-limit 10000
+
       set -s extended-keys on
       set -s extended-keys-format csi-u
       set -as terminal-features 'xterm-ghostty:extkeys'
