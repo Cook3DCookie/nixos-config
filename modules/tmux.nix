917cf3bc05fb97a06a2851d2a04631fa2206035b
Cook3DCookie <147427534+Cook3DCookie@users.noreply.github.com>
2026-04-27 19:50:40 +0200

    Configure tmux

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -10,10 +10,12 @@
     ];
     extraConfig = ''
       set-option -ga terminal-overrides ",xterm-256color:RGB"
-      set-option -g default-command "${pkgs.fish}/bin/fish -l"
+      set -g mouse on
       set -s extended-keys on
       set -s extended-keys-format csi-u
       set -as terminal-features 'xterm-ghostty:extkeys'
+
+      set -g @continuum-restore 'on'
     '';
   };
 }
