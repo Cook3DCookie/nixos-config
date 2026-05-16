ea8ec1cfbdf708bc3a582eab71be6460e09de602
Cook3DCookie <147427534+Cook3DCookie@users.noreply.github.com>
2026-05-16 18:44:59 +0200

    Add conditional check for fish config

--- modules/fish.nix
+++ modules/fish.nix
@@ -4,11 +4,13 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      if test (uname -m) = x86_64
-	echo "⚠️ Running under Rosetta (x86_64). Run 'uname -m' to check"
-	echo -ne "\033]0;Rosetta Terminal (x86_64)\007"
-      end
-
+      ${if pkgs.stdenv.isDarwin then ''
+        if test (uname -m) = x86_64
+          echo "⚠️ Running under Rosetta (x86_64). Run 'uname -m' to check"
+          echo -ne "\033]0;Rosetta Terminal (x86_64)\007"
+        end
+      '' else ""}
+      
       if command -q starship
         starship init fish | source
       end
